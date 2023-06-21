1bde6e45c90242f9d48de508fd5d39270342381b
Peter Giardiniere <pgiardiniere@gmail.com>
2023-06-20 20:37:02 -0400

    remove props and add state

--- tutorial-tic-tac-toe/src/App.js
+++ tutorial-tic-tac-toe/src/App.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React from "react"
+import { useState } from "react"
 
-function Square({ value }) {
+function Square() {
+  const [value, setValue] = useState(null)
+  
   function handleClick() {
-    console.log(`clicked ${value}`);
+    console.log(`clicked ${value}`)
   }
 
   return (
@@ -19,20 +22,20 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value={1}></Square>
-        <Square value={2}></Square>
-        <Square value={3}></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
       </div>
       <div className="board-row">
-        <Square value={4}></Square>
-        <Square value={5}></Square>
-        <Square value={6}></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
       </div>
       <div className="board-row">
-        <Square value={7}></Square>
-        <Square value={8}></Square>
-        <Square value={9}></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
       </div>
     </>
-  );
+  )
 }
