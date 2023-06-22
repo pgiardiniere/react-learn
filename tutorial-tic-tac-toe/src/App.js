7adc1ec5ba24e39d7e6e9a72924be1c829a8cb45
Peter Giardiniere <pgiardiniere@gmail.com>
2023-06-21 22:19:40 -0400

    move square state to parent board

--- tutorial-tic-tac-toe/src/App.js
+++ tutorial-tic-tac-toe/src/App.js
@@ -9,7 +9,7 @@ function Square() {
   }
 
   return (
-    <button 
+    <button
       className="square"
       onClick={handleClick}
     >
@@ -19,22 +19,24 @@ function Square() {
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  
   return (
     <>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[0]}></Square>
+        <Square value={squares[1]}></Square>
+        <Square value={squares[2]}></Square>
       </div>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[3]}></Square>
+        <Square value={squares[4]}></Square>
+        <Square value={squares[5]}></Square>
       </div>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[6]}></Square>
+        <Square value={squares[7]}></Square>
+        <Square value={squares[8]}></Square>
       </div>
     </>
   )
