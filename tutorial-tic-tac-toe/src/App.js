35ccd1b50b5949e790023f075b32a70e2f7fc508
Peter Giardiniere <pgiardiniere@gmail.com>
2023-06-23 20:16:49 -0400

    change board call signature and feed props

--- tutorial-tic-tac-toe/src/App.js
+++ tutorial-tic-tac-toe/src/App.js
@@ -7,7 +7,7 @@ function Square({value, onSquareClick}) {
   );
 }
 
-function Board() {
+function Board( {xIsNext, squares, onPlay} ) {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   const winner = calculateWinner(squares);
@@ -58,11 +58,16 @@ function Board() {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    // TODO
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board xIsNext={xIsNext} squares={squares} onPlay={handlePlay}/>
       </div>
       <div className="game-info">
         <ol>{/*TODO*/}</ol>
